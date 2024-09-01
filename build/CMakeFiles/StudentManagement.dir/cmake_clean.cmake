bd78f4cc067718ae7b74eae49370083054a7be5c
Zwe Htet Zaw <zwehtetzaw777@gmail.com>
2024-09-01 20:24:22 +0800

    CMake replaced Makefile

---
+++ build/CMakeFiles/StudentManagement.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StudentManagement.dir/main.cpp.o"
+  "CMakeFiles/StudentManagement.dir/main.cpp.o.d"
+  "CMakeFiles/StudentManagement.dir/student.cpp.o"
+  "CMakeFiles/StudentManagement.dir/student.cpp.o.d"
+  "StudentManagement"
+  "StudentManagement.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StudentManagement.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
